033986af040e1aa9e5ff01f6b5643903dacaadda
MaxBo <max.bohnet@tuhh.de>
2015-10-19 11:29:12 +0200

    add views with laea-point and polygon grid

--- src/extractiontools/laea_raster.py
+++ src/extractiontools/laea_raster.py
@@ -63,9 +63,10 @@ SELECT AddRasterConstraints('{schema}'::name,
     def additional_stuff(self):
         """
         """
-
         self.create_raster(100)
         self.create_raster(1000)
+        self.create_grid_points_and_poly(100)
+        self.create_grid_points_and_poly(1000)
 
         self.get_einwohner_hectar()
         #self.get_zensusdata_km2()
@@ -88,6 +89,9 @@ WHERE v.cellcode = z.id;
         """.format(schema=self.temp)
         self.run_query(sql, conn=self.conn0)
 
+    def get_zensusdata_km2(self):
+        """Extract Censusdata on km2 level for area"""
+        raise NotImplementedError('ToDo')
 
     def create_raster(self, pixelsize):
         """
@@ -216,57 +220,29 @@ ANALYZE {schema}.laea_vector_{pixelsize};
         else:
             return '%sm' % size
 
-    def popdens_raster(self):
-        sql = """
-DROP TABLE laea.test_raster;
-CREATE TABLE laea.test_raster
-(
-  rid serial NOT NULL,
-  rast raster,
-  CONSTRAINT test_raster_pkey PRIMARY KEY (rid),
-  CONSTRAINT enforce_height_rast CHECK (st_height(rast) = ANY (ARRAY[50, 17])),
-  CONSTRAINT enforce_max_extent_rast CHECK (st_coveredby(st_convexhull(rast), '0103000020DB0B0000010000000F0000008833A3812594504154473793B0E449418833A38125945041544737935EE149418833A3819B915041544737935EE149418833A381B98C5041544737935EE149418833A381D7875041544737935EE149418833A381F5825041544737935EE149418833A381F582504154473793B0E449418833A381F58250415447379374EE49418833A381F58250415447379338F849418833A381D78750415447379338F849418833A381B98C50415447379338F849418833A3819B9150415447379338F849418833A381259450415447379338F849418833A381259450415447379374EE49418833A3812594504154473793B0E44941'::geometry)),
-  CONSTRAINT enforce_nodata_values_rast CHECK (_raster_constraint_nodata_values(rast)::numeric(16,10)[] = '{NULL}'::numeric(16,10)[]),
-  CONSTRAINT enforce_out_db_rast CHECK (_raster_constraint_out_db(rast) = '{f}'::boolean[]),
-  CONSTRAINT enforce_same_alignment_rast CHECK (st_samealignment(rast, '0100000000000000000000594000000000000059C08833A381F58250415447379338F8494100000000000000000000000000000000DB0B000001000100'::raster)),
-  CONSTRAINT enforce_scalex_rast CHECK (st_scalex(rast)::numeric(16,10) = 100::numeric(16,10)),
-  CONSTRAINT enforce_scaley_rast CHECK (st_scaley(rast)::numeric(16,10) = (-100)::numeric(16,10)),
-  CONSTRAINT enforce_srid_rast CHECK (st_srid(rast) = 3035),
-  CONSTRAINT enforce_width_rast CHECK (st_width(rast) = ANY (ARRAY[50, 26]))
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE laea.test_raster
-  OWNER TO group_osm;
-
-
-TRUNCATE laea.test_raster;
-INSERT INTO laea.test_raster (rid, rast)
-SELECT
-r.rid,
-st_setvalues(
-st_addband(
-ST_MakeEmptyRaster(r.rast), '16BUI'::text),
-1, rv.geomval_arr
-) AS rast
-
-FROM
-laea.laea_raster_100 r,
-(
-SELECT r.rid, array_agg((v.pnt_laea, z.einwohner)::geomval) AS geomval_arr
-FROM
-laea.laea_raster_100 r,
-laea.laea_vector_100 v,
-laea.zensus_ew_hectar z
-WHERE v.cellcode = z.id
-AND v.pnt_laea && r.rast
-GROUP BY r.rid) rv
-WHERE rv.rid = r.rid;
-
-
-
+    def create_grid_points_and_poly(self, pixelsize):
         """
+        Create views of points and polygons for the given pixelsize
+
+        Parameters
+        ----------
+        pixelsize : int
+        """
+        sql = """
+CREATE OR REPLACE VIEW {schema}.grid_points_{pixelsize}
+SELECT
+  cellcode,
+  pnt AS geom
+FROM {schema}.laea_vector_{pixelsize};
+
+CREATE OR REPLACE VIEW {schema}.grid_poly{pixelsize}
+SELECT
+  cellcode,
+  geom
+FROM {schema}.laea_vector_{pixelsize};
+""".format(schema=self.schema,
+           pixelsize=pixelsize)
+        self.run_query(sql, conn=self.conn0)
 
 
 if __name__ == '__main__':
