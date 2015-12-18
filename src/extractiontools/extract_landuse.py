35328e942eb299b97503d6eef3d5627b46a7828e
MaxBoServer <bohnet@ggr-planung.de>
2015-12-18 17:00:57 +0100

    removed unused parameters from extract_landuse and extract_osm

--- src/extractiontools/extract_landuse.py
+++ src/extractiontools/extract_landuse.py
@@ -139,23 +139,6 @@ if __name__ == '__main__':
                         help="Name of destination database",
                         dest="destination_db", default='extract')
 
-    parser.add_argument("-s", '--srid', action="store",
-                        help="srid of the target database", type=int,
-                        dest="srid", default='31467')
-
-    parser.add_argument("-t", '--top', action="store",
-                        help="top", type=float,
-                        dest="top", default=54.65)
-    parser.add_argument("-b", '--bottom,', action="store",
-                        help="bottom", type=float,
-                        dest="bottom", default=54.6)
-    parser.add_argument("-r", '--right', action="store",
-                        help="right", type=float,
-                        dest="right", default=10.0)
-    parser.add_argument("-l", '--left', action="store",
-                        help="left", type=float,
-                        dest="left", default=9.95)
-
     parser.add_argument('--host', action="store",
                         help="host",
                         dest="host", default='gis.ggr-planung.de')
@@ -172,12 +155,11 @@ if __name__ == '__main__':
 
     options = parser.parse_args()
 
-    bbox = BBox(top=options.top, bottom=options.bottom,
-                left=options.left, right=options.right)
     extract = ExtractLanduse(source_db=options.source_db,
                              destination_db=options.destination_db,
-                             target_srid=options.srid,
                              recreate_db=False)
-    extract.set_login(host=options.host, port=options.port, user=options.user)
+    extract.set_login(host=options.host,
+                      port=options.port,
+                      user=options.user)
     extract.get_target_boundary_from_dest_db()
     extract.extract()
