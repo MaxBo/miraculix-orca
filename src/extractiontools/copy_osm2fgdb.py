cfd7b3d1e85c25a8d019fa3cb382b8914f5b4892
MaxBo <max.bohnet@tuhh.de>
2015-12-21 17:49:46 +0100

    multipolygon-support added

--- src/extractiontools/copy_osm2fgdb.py
+++ src/extractiontools/copy_osm2fgdb.py
@@ -49,38 +49,49 @@ CREATE OR REPLACE VIEW {schema}.{view} AS
 -- CREATE TABLE WITH POLYGONS MADE OF A SINGLE LINESTRING
 DROP TABLE osm.simple_polys;
 CREATE TABLE osm.simple_polys
-(id bigint PRIMARY KEY, geom geometry(POLYGON, {srid}));
+(id bigint PRIMARY KEY,
+geom geometry(MULTIPOLYGON, {srid}));
 
 INSERT INTO osm.simple_polys
 SELECT w.id,
-    ST_MakePolygon(w.linestring) as geom
+    st_multi(ST_MakePolygon(w.linestring)) as geom
   FROM osm.ways w
   WHERE st_IsClosed(w.linestring)
     and st_NPoints(w.linestring) > 3
 ;
 
 -- -- -- -- -- --
---  MULTIPOLYGONS - --
+--  POLYGONS WITH HOLES - --
 -- -- -- -- -- --
-DROP TABLE IF EXISTS osm.multipolygons;
-CREATE TABLE osm.multipolygons (
+DROP TABLE IF EXISTS osm.polygon_with_holes;
+CREATE TABLE osm.polygon_with_holes (
 relation_id bigint PRIMARY KEY,
 tags hstore,
 outerring_linestring geometry,
 outerring_array bigint[],
-outerring geometry,
 innerring_linestring geometry[],
-polygon geometry,
-bbox geometry(POLYGON, {srid}),
+polygon geometry(MULTIPOLYGON, {srid}),
 poly_type text NOT NULL DEFAULT 'unknown');
 
-INSERT INTO osm.multipolygons (relation_id, tags)
+-- multipolygons
+DROP TABLE IF EXISTS osm.multi_polygons;
+CREATE TABLE osm.multi_polygons
+(relation_id bigint,
+path integer,
+outerring geometry(LINESTRING, {srid}),
+polygon geometry(POLYGON, {srid}),
+ALTER TABLE osm.multi_polygons
+ADD PRIMARY KEY (relation_id, path);
+
+-- fill the table with all relations tagged "multipolygon"
+INSERT INTO osm.polygon_with_holes (relation_id, tags)
 SELECT r.id, r.tags
 FROM osm.relations r
 WHERE r.tags -> 'type' = 'multipolygon'
 AND r.tags ? 'type';
 
-UPDATE osm.multipolygons r
+-- set array of outerrings
+UPDATE osm.polygon_with_holes r
 SET outerring_array = a.arr,
 outerring_linestring = a.geom
 FROM (
@@ -88,7 +99,7 @@ SELECT r.relation_id,
 array_agg(rm.member_id) as arr,
 ST_LineMerge(ST_Collect(w.linestring)) AS geom
   FROM
-  osm.multipolygons r,
+  osm.polygon_with_holes r,
   osm.relation_members rm,
   osm.ways w
   WHERE rm.member_role = 'outer'
@@ -99,7 +110,8 @@ ST_LineMerge(ST_Collect(w.linestring)) AS geom
   GROUP BY r.relation_id) a
   WHERE a.relation_id = r.relation_id;
 
-UPDATE osm.multipolygons r
+-- set innerrings
+UPDATE osm.polygon_with_holes r
 SET innerring_linestring = b.geom_arr
 FROM (
 SELECT a.relation_id,
@@ -109,9 +121,9 @@ SELECT
 rr.relation_id,
 (st_dump(ST_LineMerge(ST_Collect(w.linestring)))).geom
   FROM
-  osm.relation_members rm,
-  osm.ways w,
-      osm.multipolygons rr
+    osm.relation_members rm,
+    osm.ways w,
+    osm.polygon_with_holes rr
   WHERE rm.member_role = 'inner'
     and rm.relation_id = rr.relation_id
     and w.id = rm.member_id
@@ -119,22 +131,19 @@ rr.relation_id,
   GROUP BY a.relation_id ) b
   WHERE b.relation_id = r.relation_id;
 
-
-SELECT relation_id, array_length(innerring_linestring, 1) FROM osm.multipolygons;
-
 -- a ring with only 3 points is flat: A-B-A (1st point = 3rd point), hence buggy
-UPDATE osm.multipolygons r
+UPDATE osm.polygon_with_holes r
 SET poly_type= 'no valid outerring' WHERE
 st_NPoints(r.outerring_linestring) < 4 -- 5 relations are buggy in italy.osm
 or r.outerring_linestring IS NULL -- about 16000 (relations between simple nodes?)
 ;
 -- the above must be done before what follows, because if less than 3 points, test may crash
-UPDATE osm.multipolygons r
+UPDATE osm.polygon_with_holes r
 SET poly_type= 'no valid outerring' WHERE
 r.poly_type = 'unknown'
 and NOT st_IsClosed(r.outerring_linestring); -- 136 are buggy in italy.osm
 
-UPDATE osm.multipolygons r
+UPDATE osm.polygon_with_holes r
 SET poly_type= 'no valid outerring' WHERE
 r.poly_type = 'unknown'
 and NOT st_IsSimple(r.outerring_linestring); -- 102 more are buggy in italy.osm
@@ -145,14 +154,14 @@ and NOT st_IsSimple(r.outerring_linestring); -- 102 more are buggy in italy.osm
 -- et us see if there is a valid innerring (or several)
 
 -- if there is no inner line, there is no valid innerring
-UPDATE osm.multipolygons r
+UPDATE osm.polygon_with_holes r
 SET poly_type= 'no valid innerring'
 WHERE r.poly_type = 'unknown'
 and r.innerring_linestring IS NULL
 ; -- 3015 more have no valid innerring
 
 -- innering must be closed
-UPDATE osm.multipolygons r
+UPDATE osm.polygon_with_holes r
 SET poly_type= 'no valid innerring'
 WHERE r.poly_type = 'unknown'
 and (NOT st_ISClosed(ST_LineMerge(ST_Collect(r.innerring_linestring))))
@@ -160,13 +169,13 @@ and (NOT st_ISClosed(ST_LineMerge(ST_Collect(r.innerring_linestring))))
 
 -- innering must be big enough
 -- all innerrings must have at least three points
-UPDATE osm.multipolygons r
+UPDATE osm.polygon_with_holes r
 SET poly_type= 'no valid innerring'
 FROM (
 SELECT r2.relation_id
 FROM (
 SELECT r.relation_id, st_NPoints(unnest(r.innerring_linestring)) n
-FROM osm.multipolygons r ) r2
+FROM osm.polygon_with_holes r ) r2
 GROUP BY r2.relation_id
 HAVING not bool_and(r2.n > 3)) r3
 WHERE r.poly_type = 'unknown'
@@ -174,19 +183,76 @@ AND r3.relation_id = r.relation_id
 ;
 
 -- check further validity of innerring: closed (multi)linestring?
-UPDATE osm.multipolygons r
+UPDATE osm.polygon_with_holes r
 SET poly_type= 'valid innerring'
 WHERE r.poly_type = 'unknown'
 and (st_ISClosed(ST_LineMerge(ST_Collect(r.innerring_linestring))))
 ;
 
---finally create the multipolygon
-UPDATE osm.multipolygons r
+-- MULTIPOLYGONS
+-- fill the multipolygon_table
+INSERT INTO osm.multi_polygons (relation_id, path, outerring)
+SELECT r.relation_id,
+(st_dump(r.outerring_linestring)).path[1],
+(st_dump(r.outerring_linestring)).geom
+FROM osm.polygon_with_holes r
+WHERE geometrytype(r.outerring_linestring) = 'MULTILINESTRING';
+
+-- build the outerring polygon
+UPDATE osm.multi_polygons m
+SET polygon = st_makepolygon(outerring)
+WHERE st_isclosed(outerring)
+AND st_npoints(outterring) > 3;
+
+-- create the full polygon
+UPDATE osm.multi_polygons m
+SET polygon = st_makepolygon(m.outerring, i2.geom_agg)
+FROM
+(SELECT
+  i.relation_id,
+  i.path,
+  array_agg(i.geom) AS geom_agg
+ FROM
+(SELECT
+  m.relation_id,
+  m.path,
+  m.polygon,
+  -- one row per innerring
+  unnest(r.innerring_linestring) AS geom
+FROM
+  osm.polygon_with_holes r,
+  osm.multi_polygons m
+WHERE r.relation_id = m.relation_id ) i
+-- check which innerring is in which outer polygon for each relation
+WHERE st_within(i.geom, i.polygon)
+-- only consider valid innerrings
+AND st_isclosed(i.geom)
+AND st_npoints(i.geom) > 3
+
+-- regroup the valid innerrings to a geometry array
+GROUP BY i.relation_id, i.path) AS i2
+WHERE m.relation_id = i2.relation_id
+AND m.path = i2.path;
+
+--finally create the Polygon
+UPDATE osm.polygon_with_holes r
 SET polygon = ST_MakePolygon(r.outerring_linestring, (r.innerring_linestring ))
 WHERE poly_type= 'valid innerring'
 and GeometryType(r.outerring_linestring) ='LINESTRING';
 
-
+-- and the Multipolygons
+UPDATE osm.polygon_with_holes r
+SET polygon = mm.geom,
+poly_type = 'multipolygon'
+FROM
+(SELECT m.relation_id,
+st_multi(st_collect(m.polygon)) AS geom
+FROM
+osm.multi_polygons m,
+osm.polygon_with_holes r
+WHERE m.relation_id = r.relation_id
+GROUP BY m.relation_id) mm
+WHERE mm.relation_id = r.relation_id;
 
 -- the complex polygons that are valid no longer need to be represented with their outerring only
 -- and not deleting those simple_polys will prevent insertion in the final polygon UNION below
@@ -197,9 +263,8 @@ DELETE FROM osm.simple_polys p WHERE p.id IN (
   and rm.relation_id
   IN
    (SELECT r.relation_id
-    FROM osm.multipolygons r
-    WHERE r.poly_type='valid innerring'
-    and ST_IsValid(r.polygon)
+    FROM osm.polygon_with_holes r
+    WHERE ST_IsValid(r.polygon)
 )
 );
 
@@ -211,22 +276,21 @@ DELETE FROM osm.simple_polys p WHERE p.id IN (
   and rm.relation_id
   IN
    (SELECT r.relation_id
-    FROM osm.multipolygons r
-    WHERE r.poly_type='valid innerring'
-    and ST_IsValid(r.polygon)
+    FROM osm.polygon_with_holes r
+    WHERE ST_IsValid(r.polygon)
 )
 );
 
 -- Create simple polygon not having (valid) innering(s)
-UPDATE osm.multipolygons r
-SET polygon = st_MakePolygon(r.outerring_linestring)
+UPDATE osm.polygon_with_holes r
+SET polygon = st_multi(st_MakePolygon(r.outerring_linestring))
 WHERE (r.poly_type = 'no valid innerring'
 OR r.poly_type = 'unknown')
 and GeometryType((r.outerring_linestring)) ='LINESTRING'
 ;
 
 -- UPDATE tags of the multipolygon with the tags from the polygon
-UPDATE osm.multipolygons r
+UPDATE osm.polygon_with_holes r
 SET tags = w2.tags || r.tags
 FROM
 (SELECT
@@ -237,8 +301,9 @@ osm.ways w,
 (SELECT
 r.relation_id,
 unnest(r.outerring_array) AS wayid
-FROM osm.multipolygons r
+FROM osm.polygon_with_holes r
 WHERE r.poly_type= 'valid innerring'
+OR r.poly_type = 'multipolygon'
 ) wr
 WHERE w.id = wr.wayid
 GROUP BY wr.relation_id ) w2
@@ -246,16 +311,11 @@ WHERE r.relation_id = w2.relation_id
 AND w2.tags != ''
 ;
 
--- set the bbox for spatial queries
-UPDATE osm.multipolygons
-SET bbox = ST_Envelope(polygon);
-
 CREATE OR REPLACE VIEW osm.polygons
 AS
 SELECT
 p.id,
 p.geom,
-w.bbox,
 w.tags
 FROM osm.simple_polys p, osm.ways w
 WHERE p.id = w.id
@@ -264,15 +324,16 @@ UNION ALL
 
 SELECT m.relation_id AS id,
 m.polygon AS geom,
-m.bbox,
 m.tags
-FROM osm.multipolygons m
+FROM osm.polygon_with_holes m
 WHERE m.poly_type != 'no valid outerring';
 
-CREATE INDEX poly_tags_idx ON osm.multipolygons
+ANALYZE osm.polygon_with_holes;
+
+CREATE INDEX poly_tags_idx ON osm.polygon_with_holes
 USING gist(tags);
-CREATE INDEX poly_geom_idx ON osm.multipolygons
-USING gist(bbox);
+CREATE INDEX poly_geom_idx ON osm.polygon_with_holes
+USING gist(polygon);
 
         """
         self.run_query(sql, srid=self.options.target_srid)
