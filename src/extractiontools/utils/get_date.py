53809c9ca3edd0e5a614f2885cb8a2fc03f98222
MaxBo <bohnet@ggr-planung.de>
2016-11-16 19:44:30 +0100

    fix bug in Date.get_timestamp

--- src/extractiontools/utils/get_date.py
+++ src/extractiontools/utils/get_date.py
@@ -52,15 +52,9 @@ class Date(datetime.date):
 
     def get_timestamp(self, time_to_convert):
         """
-        convert a date to a timestamd
+        convert a date to a timestamp
         """
-        time_format = '%H:%M:%S'
-        dateString = format(self, '%Y-%m-%d')
-        try:
-            time_str = time.strftime(time_format, time_to_convert)
-            return '%s %sCEST' %(dateString, time_str)
-        except:
-            return None
+        return get_timestamp2(time_to_convert, date=self)
 
     def shift_day(self, days=1):
         """adds or substracts the number of days"""
@@ -71,20 +65,42 @@ class Date(datetime.date):
     def day(self):
         return self.timetuple().tm_mday
 
-def get_timestamp2(time_to_convert):
+def get_timestamp2(time_to_convert, date=None):
+    """
+    get a timestamp for the time_to_convert
+
+    Parameters
+    ----------
+    time_to_convert : datetime-object
+    date : Date-instance (optional)
+
+    if date is None, then use the date of the time_to_convert,
+    otherwise use the date provided by `date' together with
+    the time provided by `time_to_convert'
+
+    Returns
+    -------
+    timestamp : str
+    """
     t = time_to_convert
+    if not t:
+        return ''
 
-    if t:
-        dt = datetime.datetime(t.tm_year,
-                               t.tm_mon,
-                               t.tm_mday,
-                               t.tm_hour,
-                               t.tm_min,
-                               t.tm_sec)
+    if date is None:
+        date = Date(year = t.tm_year,
+                    month = t.tm_mon,
+                    day = t.tm_mday)
 
-        # check Sommerzeit
-        local = berlin.localize(dt, is_dst=False)
-        tz = 'CEST' if local.dst() else 'CET'
-        fmt = '%Y-%m-%d %H:%M:%S{tz}'.format(tz=tz)
+    dt = datetime.datetime(date.year,
+                           date.month,
+                           date.day,
+                           t.tm_hour,
+                           t.tm_min,
+                           t.tm_sec)
 
-        return dt.strftime(fmt)
+    # check Sommerzeit
+    local = berlin.localize(dt, is_dst=False)
+    tz = 'CEST' if local.dst() else 'CET'
+    fmt = '%Y-%m-%d %H:%M:%S{tz}'.format(tz=tz)
+
+    return dt.strftime(fmt)
