72c5baef30472859443d6b9a14cdde60744bc7b2
MaxBo <bohnet@ggr-planung.de>
2020-03-03 11:32:15 +0100

    upgrade OTP to 1.4

--- extractiontools/src/extractiontools/otp_config.py
+++ extractiontools/src/extractiontools/otp_config.py
@@ -1,2 +1,2 @@
-OTP_JAR='/opt/OpenTripPlanner/otp-ggr-stable.jar'
-JAVA='/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java'
+OTP_JAR = '/opt/OpenTripPlanner/otp-1.4.0-shaded.jar'
+JAVA = '/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java'
