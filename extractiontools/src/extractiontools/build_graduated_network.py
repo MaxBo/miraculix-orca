45607b073fce04b2a6f63a9c8926d6d980d929ee
Max Bohnet <bohnet@ggr-planung.de>
2021-09-29 17:31:14 +0200

    include roads served by busses

--- extractiontools/src/extractiontools/build_graduated_network.py
+++ extractiontools/src/extractiontools/build_graduated_network.py
@@ -38,19 +38,28 @@ SELECT
   min(wtl.linktype_id) linktype_id,
   min(lt.road_category)::char(1) category
 FROM
-osm.ways w,
+osm.ways w
+LEFT JOIN osm.relation_members rm ON (rm.member_id = w.id AND rm.member_type = 'W')
+LEFT JOIN osm.relations r ON (rm.relation_id = r.id
+AND r.tags ? 'route'
+AND r.tags -> 'route' IN ('bus','trolleybus'))
+,
 classifications.linktypes lt,
 classifications.waytype2linktype wtl,
 classifications.wt2lt_construction wtc,
 (SELECT ST_Transform(ST_GeomFromEWKT('SRID={srid};{wkt_da}'), {target_srid}) AS geom) da,
 (SELECT ST_Transform(ST_GeomFromEWKT('SRID={srid};{wkt_la}'), {target_srid}) AS geom) la
-WHERE (w.tags @> wtl.tags
+WHERE
+(w.tags @> wtl.tags
 -- Nimm auch geplante oder im Bau befindliche Straßen mit
 OR (w.tags @> wtc.tag1 AND w.tags @> wtc.tag2))
 AND wtl.linktype_id=lt.id
 AND wtc.linktype_id=lt.id
-AND ((lt.road_category <= 'B' AND st_intersects(w.linestring, la.geom))
-OR st_intersects(w.linestring, da.geom))
+AND (
+(( r.id IS NOT NULL OR lt.road_category <= 'B')
+AND st_intersects(w.linestring, la.geom))
+OR st_intersects(w.linestring, da.geom)
+)
 GROUP BY w.id;
 CREATE INDEX streets_idx ON "{network}".streets USING btree(id);
 ANALYZE "{network}".streets;
