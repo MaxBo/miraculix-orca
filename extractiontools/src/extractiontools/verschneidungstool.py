ea7e11bcc023394fbbad870ea49cd558a45db9a0
Max Bohnet <bohnet@ggr-planung.de>
2022-11-18 07:12:40 +0100

    add indices to prepare_verschneidungstool

--- extractiontools/src/extractiontools/verschneidungstool.py
+++ extractiontools/src/extractiontools/verschneidungstool.py
@@ -29,11 +29,11 @@ class PrepareVerschneidungstool(Extract):
     def final_stuff(self):
         """Final steps in the destiantion db"""
         self.create_views()
+        self.create_indices()
 
     def create_views(self):
         """
-        create views for zensus data and
-        add a geometry index to views
+        create views for current year
         """
         sql = f"""
 CREATE OR REPLACE VIEW {self.schema}.current_year (
@@ -43,5 +43,68 @@ SELECT s.jahr
 FROM {self.schema}.scenarios_available s,
     {self.schema}.current_scenario c
 WHERE s.scenario = c.scenario;
+        """
+        self.run_query(sql)
+
+    def create_indices(self):
+        """
+        Add indeces
+        """
+        sql = f"""
+ALTER TABLE {self.schema}.current_scenario
+ADD PRIMARY KEY(scenario);
+ALTER TABLE {self.schema}.scenarios_available
+ADD PRIMARY KEY(scenario);
+ALTER TABLE {self.schema}.areas_available
+ADD PRIMARY KEY(id);
+ALTER TABLE {self.schema}.schemata_available
+ADD PRIMARY KEY(name);
+ALTER TABLE {self.schema}.haltestellen_available
+ADD PRIMARY KEY(name);
+CREATE UNIQUE INDEX haltestellen_available_id_key ON {self.schema}.haltestellen_available
+  USING btree (id);
+ALTER TABLE {self.schema}.projcs2srid
+ADD PRIMARY KEY(name);
+ALTER TABLE {self.schema}.projections_available
+ADD PRIMARY KEY(srid),
+ADD CONSTRAINT projections_available_srid_fkey FOREIGN KEY (srid)
+    REFERENCES public.spatial_ref_sys(srid)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+    NOT DEFERRABLE
+;
+ALTER TABLE {self.schema}.queries
+ADD PRIMARY KEY(id);
+ALTER TABLE {self.schema}.resulttables_available
+ADD PRIMARY KEY(schema_table);
+ALTER TABLE {self.schema}.table_categories
+ADD PRIMARY KEY(id);
+CREATE UNIQUE INDEX table_categories_name_key ON {self.schema}.table_categories
+  USING btree (name);
+ALTER TABLE {self.schema}.tables_to_download
+ADD PRIMARY KEY(name);
+ALTER TABLE {self.schema}.columns_available
+ADD PRIMARY KEY(id),
+ADD CONSTRAINT columns_available_fk FOREIGN KEY (table_type)
+    REFERENCES {self.schema}.table_categories(name)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE
+    NOT DEFERRABLE,
+ADD CONSTRAINT columns_available_fk1 FOREIGN KEY (resulttable)
+    REFERENCES {self.schema}.resulttables_available(schema_table)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
+    NOT DEFERRABLE
+;
+ALTER TABLE {self.schema}.column_definitions
+ADD PRIMARY KEY(table_category),
+ADD CONSTRAINT column_definitions_fk FOREIGN KEY (table_category)
+    REFERENCES {self.schema}.table_categories(name)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+    NOT DEFERRABLE
+;
+CREATE UNIQUE INDEX columns_available_column_idx ON {self.schema}.columns_available
+  USING btree ("column", resulttable );
         """
         self.run_query(sql)
